34c0f6c8264988564713aef15c6317a6ce9730e9
Francisco Carrasco Girbés <francagir@hotmail.com>
2025-12-10 21:03:07 +0100

    Adds Initialization
    LCD

--- Main/Main.ino
+++ Main/Main.ino
@@ -2,7 +2,8 @@
 
 //Configuración componentes
 	//LCD
-LiquidCrystal_I2C lcd(0x20,16,2);
+LiquidCrystal_I2C lcd(0x20,16,2); //16 letras, 2 filas 
+								 // 4 numeros 12 extra (Eq1, Eq2), "a" a mitad, etc.
 	//LEDS
 const int ledTeam1 =3;
 const int ledTeam2 =4;
@@ -17,19 +18,30 @@ int pointTeam1=0;
 int pointTeam2=0;
 
 
-
 void setup()
 {
- lcd.init();         // Inicializa la pantalla LCD
+ lcd.init();   
   lcd.backlight();
   lcd.setCursor(0, 0); 
   lcd.print("Empieza");
   lcd.setCursor(0, 1);
   lcd.print("el partido!");
-  
+  delay(2000);
+  lcd.clear();  //ACORDAROS DE ESTA LINEA PARA LIMPIAR LA PANTALLA ANTES DE AÑADIR MÁS COSAS O NUEVAS
+  WritePoints(pointTeam1, pointTeam2);
 }
 
 void loop()
 {
  
 }
+void SetTeam1Pts(){
+}
+void SetTeam2Pts(){
+}
+void WritePoints(int pts1, int pts2){
+  lcd.setCursor(0, 0); 
+  lcd.print("PARTIDO");
+  lcd.setCursor(0, 1);
+  lcd.print("Eq1: "+String(pts1)+ "  "+ "Eq2:"+String(pts2)); //Hay que añadir el string(variable) para que imprima
+} 
