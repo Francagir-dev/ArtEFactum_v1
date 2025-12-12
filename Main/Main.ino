f27906f4c54153329142f47a89f2d027d171442a
Francisco Carrasco Girbés <60694098+Francagir-dev@users.noreply.github.com>
2025-12-12 18:30:01 +0100

    Optimize code

--- Main/Main.ino
+++ Main/Main.ino
@@ -47,7 +47,36 @@ void setup()
 
 void loop()
 {
-  
+ ControlTeam1();
+ ControlTeam2(); 
+
+
+
+ 
+}
+void SumarTeam1(int pts){
+  pointTeam1+=pts;
+  WritePoints(pointTeam1, pointTeam2);
+  tone(buzzer, 800, 80);
+}
+
+void RestarTeam1(int pts){
+  if (pointTeam1 > 0) pointTeam1-=pts;
+  WritePoints(pointTeam1, pointTeam2);
+}
+
+void SumarTeam2(int pts){
+  pointTeam2+=pts;
+  WritePoints(pointTeam1, pointTeam2);
+  tone(buzzer, 800, 80);
+}
+
+void RestarTeam2(int pts){
+  if (pointTeam2 > 0) pointTeam2-=pts;
+  WritePoints(pointTeam1, pointTeam2);
+ 
+}
+void ControlTeam1(){
   //   LECTURA BOTÓN TEAM 1
   bool currentState1 = digitalRead(Team1);
 
@@ -59,21 +88,22 @@ void loop()
   // Cuando se suelta (LOW -> HIGH)
   if (lastState1 == LOW && currentState1 == HIGH) {
     unsigned long duration = millis() - pressTeam1;
-
     if (duration >= longPressTime) {
       // PULSACIÓN LARGA TEAM 1 
-      RestarTeam1();
+      if(pointTeam1<14) RestarTeam1(1);
+      else RestarTeam1(2);
     } else {
       // PULSACIÓN CORTA TEAM 1 
-      SumarTeam1();
+      if(pointTeam1<12) SumarTeam1(1);
+	  else SumarTeam1(2);
     }
   }
-
   lastState1 = currentState1;
+	
+}
 
-
-
-  //   LECTURA BOTÓN TEAM 2
+void ControlTeam2(){
+ //   LECTURA BOTÓN TEAM 2
 
   bool currentState2 = digitalRead(Team2);
 
@@ -86,41 +116,25 @@ void loop()
 
     if (duration >= longPressTime) {
       //  PULSACIÓN LARGA TEAM 2 
-      RestarTeam2();
+     if(pointTeam1<14) RestarTeam2(1);
+      else RestarTeam2(2);
     } else {
       //  PULSACIÓN CORTA TEAM 2 
-      SumarTeam2();
+     if(pointTeam1<12) SumarTeam2(1);
+	  else SumarTeam2(2);
     }
   }
 
   lastState2 = currentState2;
-}
-void SumarTeam1(){
-  pointTeam1++;
-  WritePoints(pointTeam1, pointTeam2);
-  tone(buzzer, 800, 80);
-}
-
-void RestarTeam1(){
-  if (pointTeam1 > 0) pointTeam1--;
-  WritePoints(pointTeam1, pointTeam2);
-
-}
-
-void SumarTeam2(int pts){
-  pointTeam2+=pts;
-  WritePoints(pointTeam1, pointTeam2);
-  tone(buzzer, 800, 80);
-}
-
-void RestarTeam2(){
-  if (pointTeam2 > 0) pointTeam2--;
-  WritePoints(pointTeam1, pointTeam2);
- 
+	
 }
 void WritePoints(int pts1, int pts2){
-  lcd.setCursor(0, 0); 
-  lcd.print("PARTIDO");
-  lcd.setCursor(0, 1);
-  lcd.print("Eq1: "+String(pts1)+ "  "+ "Eq2: "+String(pts2)); //Hay que añadir el string(variable) para que imprima
+  lcd.setCursor(0, 0); //X de 0 a
+  lcd.print("EQUIP1 | EQUIP2");
+  lcd.setCursor(2, 1); 
+  lcd.print(String(pointTeam1)); //Hay que añadir el string(variable) para que imprima (Por casteo de variable). 
+  lcd.setCursor(7, 1);
+  lcd.print("|");
+  lcd.setCursor(11, 1);
+  lcd.print(String(pointTeam2));
 } 
